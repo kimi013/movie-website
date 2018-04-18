e6b2faeb03e0d8fe615bf7f2c4d55473a052e175
liuyang <yliu234@126.com>
2018-04-18 23:24:16 +0800

    会话持久逻辑预处理

--- app.js
+++ app.js
@@ -42,16 +42,20 @@ app.use(session({
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-// 首页
-app.get('/', function (req, res) {
-    console.log('user in session: ');
-    console.log(req.session.user);
-
+// 预处理user
+app.use(function (req, res, next) {
     var _user = req.session.user;
 
     if (_user) {
         app.locals.user = _user;     // todo 全局变量赋值
     }
+    return next();
+});
+
+// 首页
+app.get('/', function (req, res) {
+    console.log('user in session: ');
+    console.log(req.session.user);
 
     Movie.fetch(function (err, movies) {
         if (err) {
