a8abff404067b04f343f9f83efb804123d6acc17
liuyang <yliu234@126.com>
2018-05-13 20:20:32 +0800

    搜索，公用列表及分页

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,3 @@
-var mongoose = require('mongoose');
 var Movie = require('../models/movie');
 var Category = require('../models/category');
 
@@ -30,37 +29,63 @@ exports.index = function (req, res) {
 // 搜索
 exports.search = function (req, res) {
     var catId = req.query.cat;
-    var page = parseInt(req.query.p, 10);
+    var q = req.query.q;
+    var page = parseInt(req.query.p, 10) || 1;
     var count = 2;
     var index = (page - 1) * count;
 
-    Category
-        .find({
-            _id: catId
-        })
-        .populate({
-            path: 'movies',
-            // options: {
-            //     limit: 2,
-            //     // skip: index
-            // }
-        })
-        .exec(function (err, categories) {
-            if (err) {
-                console.log(err)
-            }
+    if (catId) {
+        Category
+            .find({
+                _id: catId
+            })
+            .populate({
+                path: 'movies',
+                // options: {
+                //     limit: 2,
+                //     // skip: index
+                // }
+            })
+            .exec(function (err, categories) {
+                if (err) {
+                    console.log(err)
+                }
 
-            var category = categories[0] || {};
-            var movies = category.movies || [];
-            var results = movies.slice(index, index + count);
+                var category = categories[0] || {};
+                var movies = category.movies || [];
+                var results = movies.slice(index, index + count);
 
-            res.render('results', {
-                title: 'imooc 结果列表页',
-                keyword: category.name,
-                currentPage: page,
-                totalPage: Math.ceil(movies.length / count),
-                query: 'cat=' + catId,
-                movies: results
+                res.render('results', {
+                    title: 'imooc 结果列表页',
+                    keyword: category.name,
+                    currentPage: page,
+                    totalPage: Math.ceil(movies.length / count),
+                    query: 'cat=' + catId,
+                    movies: results
+                });
             });
-        });
+    } else {
+        Movie
+            .find({
+                title: new RegExp(q + '.*', 'i')
+            })
+            .exec(function(err, movies) {
+                if (err) {
+                    console.log(err)
+                }
+
+                var results = movies.slice(index, index + count);
+
+                res.render('results', {
+                    title: 'imooc 结果列表页',
+                    keyword: q,
+                    currentPage: page,
+                    query: 'q=' + q,
+                    totalPage: Math.ceil(movies.length / count),
+                    movies: results
+                });
+            })
+    }
+
+
 };
